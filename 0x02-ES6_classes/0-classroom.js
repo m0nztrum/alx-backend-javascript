a7cacf641259f8edd05ac89abf9b3b4c305e1a28
delafiado@gmail.com <delafiado@gmail.com>
2024-04-12 00:37:38 +0000

    task 0

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,5 +1,12 @@
+/**
+ *  Representing a class room
+ */
 export default class ClassRoom {
-    constructor(maxStudentsSize) {
-        this._maxStudentsSize = maxStudentsSize;
-    }
+  /**
+   *  class representing a classroom
+   *  @param {Number} maxStudentsSize - maximum number of students in the class
+   */
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
 }
