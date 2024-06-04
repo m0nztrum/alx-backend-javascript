e1e43fa557343099cb0668d8c6fec5fab38be673
Dragonixj <delafiado@gmail.com>
2024-06-04 18:45:11 +0000

    initial commit & task 0

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,11 @@
+/*
+ * Display the message in the console STDOUT
+ * @param {string} message - The message to be displayed
+ * @return {string} returns a string value
+ */
+
+const displayMessage = (message) => {
+  console.log(message);
+};
+
+module.exports = displayMessage;
