c8d278c45416779c543c93047ccd18d8a91ac02c
Dragonixj <delafiado@gmail.com>
2024-06-05 11:13:16 +0000

    task 2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,5 +1,6 @@
 /*
  * Counts students in a CSV data file
+ * @param {string} path - path to the csv file
  */
 
 const fs = require('fs');
@@ -10,6 +11,44 @@ const countStudents = (path) => {
     data = fs.readFileSync(path, 'utf8');
   } catch (err) {
     throw new Error('Cannot load the database');
+  }
+
+  const lines = data
+    .trim()
+    .split('\n')
+    .filter((line) => line.trim() !== '');
+  if (lines.length <= 1) {
+    return;
+  }
+
+  const studentGroups = {};
+  const dbFields = lines[0].split(',');
+  const studentNames = dbFields.slice(0, dbFields.length - 1);
+
+  for (const line of lines.slice(1)) {
+    const studentRecord = line.split(',');
+    const studentValues = studentRecord.slice(0, studentRecord.length - 1);
+    const field = studentRecord[studentRecord.length - 1];
+    if (!Object.keys(studentGroups).includes(field)) {
+      studentGroups[field] = [];
+    }
+    const studentEntries = studentNames.map((Name, idx) => [
+      Name,
+      studentValues[idx],
+    ]);
+    studentGroups[field].push(Object.fromEntries(studentEntries));
+  }
+
+  const totalStudents = Object.values(studentGroups).reduce(
+    (sum, group) => sum + group.length,
+    0,
+  );
+  console.log(`Number of students: ${totalStudents}`);
+  for (const [field, group] of Object.entries(studentGroups)) {
+    const studentNames = group.map((student) => student.firstname).join(', ');
+    console.log(
+      `Number of students in ${field}: ${group.length}. List: ${studentNames}`,
+    );
   }
 };
 
