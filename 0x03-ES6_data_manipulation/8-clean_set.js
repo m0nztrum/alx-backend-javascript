c17bf89ab14b0a6b621acf1dd5c96966a92135bf
m0nztrum <delafiado@gmail.com>
2024-07-04 15:05:03 +0000

    done

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,12 @@
+function cleanSet(set, startString) {
+  if (!startString || typeof startString !== 'string') {
+    return '';
+  }
+
+  return [...set]
+    .filter((item) => typeof item === 'string' && item.startsWith(startString))
+    .map((str) => str.slice(startString.length))
+    .join('-');
+}
+
+export default cleanSet;
