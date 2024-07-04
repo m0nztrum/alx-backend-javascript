1f09a0de817c262131e142493222c3ac90ca2e89
m0nztrum <delafiado@gmail.com>
2024-07-04 15:22:22 +0000

    done

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,16 @@
+const weakMap = new WeakMap();
+
+function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
+  }
+
+  const count = weakMap.get(endpoint) + 1;
+
+  if (count >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+  weakMap.set(endpoint, count);
+}
+
+export { weakMap, queryAPI };
